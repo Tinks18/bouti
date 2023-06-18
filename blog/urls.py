3c4b107da7f8119f7ab51679b12752e52e78a458
Tinks18 <tinutani9@gmail.com>
2023-06-18 19:42:58 +0000

    corrected checkout css and blog templates

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='blog'),
+    #   path('', views.post_list, name='blog'),
+    # path("", views.PostList.as_view(), name="home"),
+    path('blog/', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
    ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "blog/blog.html"
+    paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+             },
+        )
 
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html')
+# class PostList(generic.View):
+#     def get(self, request, *args, **kwargs):
+#         return render(
+#             request,
+#             'blog.html',
+#             # {
+#             #     'blog_form': BLOGForm()
+#             # }
+#         )
+# class PostList(generic.ListView):
+#     model = Post
+#     queryset = Post.objects.filter(status=1).order_by("-created_on")
+#     template_name = "blog.html"
+#     paginate_by = 6
+
+
+
+
+
+
+
+
+# def post_list(request):
+#     return render(request, 'blog/post_list.html')
