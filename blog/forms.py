37da3da1a9efa8b13c78bbf0b2162bf3ac1e0a09
Tinks18 <tinutani9@gmail.com>
2023-06-19 00:59:01 +0000

    added favicon icons

--- blog/forms.py
+++ blog/forms.py
@@ -10,34 +10,3 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
-
-
-# Form to create a blogpost.
-class BlogPost(forms.ModelForm):
-    """
-    Creates a form based on the Post model
-    """
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-
-# Form for user to create a blog post.
-class CreatePost(forms.ModelForm):
-    """
-    Creates a form based on the Post model
-    """
-    class Meta:
-        model = Post
-        fields = (
-            'title',
-            'featured_image',
-            'snippet', 'content')
-
-    widgets = {
-        'title': forms.TextInput(attrs={'class': 'form-control',
-                                        'label': 'name'}),
-        'feature_image': forms.Select(attrs={'class': 'form-control'}),
-        'excerpt': forms.Select(attrs={'placeholder': 'excerpt of blog'}),
-        'content': forms.TextInput(attrs={'placeholder': 'blog post'}),
-    }
